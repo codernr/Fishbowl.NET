78dcb8528fc5c669b5745afcf5621ae87e3447a7
codernr <zoltan.csore@protonmail.com>
2021-03-29 23:00:18 +0200

    #2 Fixes in data types

--- Shared/Data.cs
+++ Shared/Data.cs
@@ -9,16 +9,16 @@ namespace Fishbowl.Net.Shared.Data
 
     public record Score(Word word, DateTimeOffset timestamp);
 
-    public record Period(int Length, Player player)
+    public record Period(TimeSpan Length, Player Player)
     {
-        public DateTimeOffset StartedAt { get; set; }
+        public DateTimeOffset? StartedAt { get; set; }
 
-        public DateTimeOffset FinishedAt { get; set; }
+        public DateTimeOffset? FinishedAt { get; set; }
 
         public ICollection<Score> Scores { get; } = new List<Score>();
     }
 
-    public record Round(string Type, Stack<Word> wordList)
+    public record Round(string Type, Stack<Word> WordList)
     {
         public ICollection<Period> Periods { get; } = new List<Period>();
     }
