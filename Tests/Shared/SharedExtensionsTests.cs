cd47e08c980762002ac6f7a7b96b6db7fa755969
codernr <zoltan.csore@protonmail.com>
2021-03-29 22:01:05 +0200

    #2 Create test for Distribute extension method

--- Tests/Shared/SharedExtensionsTests.cs
+++ Tests/Shared/SharedExtensionsTests.cs
@@ -1,4 +1,5 @@
-using System;
+using System.Linq;
+using Fishbowl.Net.Shared;
 using Xunit;
 
 namespace Fishbowl.Net.Tests.Shared
@@ -8,7 +9,20 @@ namespace Fishbowl.Net.Tests.Shared
         [Fact]
         public void TestDistribute()
         {
+            var list = new[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j" };
 
+            var list2 = list.Distribute(2).ToArray();
+
+            Assert.Equal(2, list2.Length);
+            Assert.Equal(new[] { "a", "c", "e", "g", "i" }, list2[0]);
+            Assert.Equal(new[] { "b", "d", "f", "h", "j" }, list2[1]);
+
+            var list3 = list.Distribute(3).ToArray();
+
+            Assert.Equal(3, list3.Length);
+            Assert.Equal(new[] { "a", "d", "g", "j" }, list3[0]);
+            Assert.Equal(new[] { "b", "e", "h" }, list3[1]);
+            Assert.Equal(new[] { "c", "f", "i" }, list3[1]);
         }
     }
 }
