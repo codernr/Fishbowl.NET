a2b8da0437a9cd3976a48a302655867059a492fe
codernr <zoltan.csore@protonmail.com>
2021-03-29 19:10:08 +0200

    Resolve #1

---
+++ Shared/Data.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+namespace Fishbowl.Net.Shared.Data
+{
+    public record Word(string Value)
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+    }
+
+    public record Player(string Name, IEnumerable<Word> Words)
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+    }
+
+    public record Score(Word word, DateTimeOffset timestamp);
+
+    public record Period(int Length, Player player)
+    {
+        public DateTimeOffset StartedAt { get; set; }
+
+        public DateTimeOffset FinishedAt { get; set; }
+
+        public ICollection<Score> Scores { get; } = new List<Score>();
+    }
+
+    public record Round(string Type)
+    {
+        public ICollection<Period> Periods { get; } = new List<Period>();
+    }
+
+    public record Team(int Id, IList<Player> Players);
+
+    public record Game(Guid Id, IList<Team> Teams, IList<Round> Rounds);
+}
