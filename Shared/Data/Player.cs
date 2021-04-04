f809f09cdd202cd5b3ff0dd6f9f8a6a7ba6f2857
codernr <zoltan.csore@protonmail.com>
2021-04-04 15:30:29 +0200

    #5 Override player equality for simpler handling

--- Shared/Data/Player.cs
+++ Shared/Data/Player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Fishbowl.Net.Shared.Data
 {
-    public class Player
+    public class Player : IEquatable<Player>
     {
         public Guid Id { get; private set; }
         
@@ -13,5 +13,28 @@ namespace Fishbowl.Net.Shared.Data
         
         public Player(Guid id, string name, IEnumerable<Word> words) =>
             (this.Id, this.Name, this.Words) = (id, name, words);
+
+        public bool Equals(Player? other) => other is not null && other.Id == this.Id;
+
+        public override bool Equals(object? obj) => this.Equals(obj as Player);
+
+        public override int GetHashCode() => this.Id.GetHashCode();
+
+        public static bool operator ==(Player lhs, Player rhs)
+        {
+            if (lhs is null)
+            {
+                if (rhs is null)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            
+            return lhs.Equals(rhs);
+        }
+
+        public static bool operator !=(Player lhs, Player rhs) => !(lhs == rhs);
     }
 }
