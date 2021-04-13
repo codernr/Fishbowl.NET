f09f560b12f2329e48185c50c913b39963652b9b
codernr <zoltan.csore@protonmail.com>
2021-04-13 22:01:47 +0200

    #9 Handle local validation

--- Client/Components/States/RoundTypes.razor.cs
+++ Client/Components/States/RoundTypes.razor.cs
@@ -9,6 +9,8 @@ namespace Fishbowl.Net.Client.Components.States
         [Parameter]
         public EventCallback<string[]> OnRoundTypesSet { get; set; } = default!;
 
+        private bool IsValid => this.options.Any(option => option.selected);
+
         private List<(string name, bool selected)> options = new()
         {
             ("Taboo", true),
@@ -23,7 +25,10 @@ namespace Fishbowl.Net.Client.Components.States
                 .Select(option => option.name)
                 .ToArray();
 
-        private void ToggleOption(int id) =>
+        private void ToggleOption(int id)
+        {
             this.options[id] = (this.options[id].name, !this.options[id].selected);
+            this.Update();
+        }
     }
 }
