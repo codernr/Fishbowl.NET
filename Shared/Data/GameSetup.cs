849a3a72445c38626ba6988c239402cefe94c6e3
codernr <zoltan.csore@protonmail.com>
2021-05-07 17:21:48 +0200

    #16 Create PlayerCount state

--- Shared/Data/GameSetup.cs
+++ Shared/Data/GameSetup.cs
@@ -2,6 +2,8 @@ namespace Fishbowl.Net.Shared.Data
 {
     public class GameSetup
     {
+        public int PlayerCount { get; set; } = 4;
+        
         public int WordCount { get; set; } = 2;
 
         public int TeamCount { get; set; } = 2;
