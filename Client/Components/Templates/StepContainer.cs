0659058620e3b4e88e7c0967ece909eab2b8363a
codernr <zoltan.csore@protonmail.com>
2021-05-07 20:16:58 +0200

    #17 Add step parameters

--- Client/Components/Templates/StepContainer.cs
+++ Client/Components/Templates/StepContainer.cs
@@ -5,8 +5,14 @@ namespace Fishbowl.Net.Client.Components.Templates
     public partial class StepContainer
     {
         [Parameter]
+        public string Title { get; set; } = default!;
+
+        [Parameter]
         public string Subtitle { get; set; } = default!;
 
+        [Parameter]
+        public int TotalSteps { get; set; }
+        
         [Parameter]
         public int Step { get; set; } = 1;
 
