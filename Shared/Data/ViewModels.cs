54c1536e399f48bd7cc2c59b010e29473a55cc65
codernr <zoltan.csore@protonmail.com>
2021-05-12 23:08:24 +0200

    #19 Add Word and Score viewmodel

--- Shared/Data/ViewModels.cs
+++ Shared/Data/ViewModels.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json.Serialization;
 
 namespace Fishbowl.Net.Shared.Data.ViewModels
@@ -22,15 +23,28 @@ namespace Fishbowl.Net.Shared.Data.ViewModels
         RoundViewModel Round, PlayerViewModel Player, double LengthInSeconds, DateTimeOffset StartedAt) :
         PeriodSetupViewModel(Round, Player, LengthInSeconds);
 
-    public record PeriodSummaryViewModel(PlayerViewModel Player, List<string> Scores);
+    public record PeriodSummaryViewModel(PlayerViewModel Player, List<ScoreViewModel> Scores);
+
+    public record WordViewModel(Guid Id, string Value);
+
+    public record ScoreViewModel(WordViewModel Word, DateTimeOffset Timestamp);
 
     public static class ViewModelExtensions
     {
+        public static PlayerViewModel Map(this Player player) => new(player.Id, player.Name);
+
         public static PeriodSetupViewModel Map(this Period period, Round round) =>
             new(new(round.Type), new(period.Player.Id, period.Player.Name), period.LengthInSeconds);
 
         public static PeriodRunningViewModel MapRunning(this Period period, Round round) =>
             new(new(round.Type), new(period.Player.Id, period.Player.Name), period.LengthInSeconds,
             period.StartedAt ?? throw new InvalidOperationException());
+
+        public static PeriodSummaryViewModel Map(this Period period) =>
+            new(period.Player.Map(), period.Scores.Select(score => score.Map()).ToList());
+
+        public static WordViewModel Map(this Word word) => new(word.Id, word.Value);
+
+        public static ScoreViewModel Map(this Score score) => new(score.Word.Map(), score.Timestamp);
     }
 }
