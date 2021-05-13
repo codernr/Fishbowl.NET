c41c6327139076a3533682dbe3428237e75799a7
codernr <zoltan.csore@protonmail.com>
2021-05-13 02:18:31 +0200

    #19 Fix equality problem

--- Shared/Data/Word.cs
+++ Shared/Data/Word.cs
@@ -2,13 +2,5 @@ using System;
 
 namespace Fishbowl.Net.Shared.Data
 {
-    public class Word
-    {
-        public Guid Id { get; private set; }
-        
-        public string Value { get; private set; }
-
-        public Word(Guid id, string value) =>
-            (this.Id, this.Value) = (id, value);
-    }
+    public record Word(Guid Id, string Value);
 }
