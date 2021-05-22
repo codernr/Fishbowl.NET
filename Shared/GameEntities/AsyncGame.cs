65db9d4482e5d9225c5437fb8e5e10a65192e8f0
codernr <zoltan.csore@protonmail.com>
2021-05-23 01:37:59 +0200

    #21 Move AsyncGame to GameEntities

--- Shared/GameEntities/AsyncGame.cs
+++ Shared/GameEntities/AsyncGame.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Fishbowl.Net.Shared.GameEntities;
 
-namespace Fishbowl.Net.Shared
+namespace Fishbowl.Net.Shared.GameEntities
 {
     public class AsyncGame
     {
