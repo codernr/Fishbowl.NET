0b76a8c71ca9766f4bf5a94c4d571b969cfa763c
codernr <zoltan.csore@protonmail.com>
2021-06-14 15:02:49 +0200

    #31 Update build to node script, remove unused packages, update csproj

--- Client.Shared/Npm/build.js
+++ Client.Shared/Npm/build.js
@@ -1,14 +1,55 @@
-const hashFiles = require('hash-files');
 const fs = require('fs');
+const sass = require('sass');
+const autoprefixer = require('autoprefixer');
+const postcss = require('postcss');
+const { PurgeCSS } = require('purgecss');
+const { createHash } = require('crypto');
 
-const PATH = './../wwwroot/css/styles.css';
+const PATH = './../wwwroot/css/';
+const FILE_PATH = `${PATH}styles.css`;
+const HASH_LENGTH = 32;
+const REGEXP = new RegExp(`styles.[a-z0-9]{${HASH_LENGTH}}.css$`, 'g');
 
-hashFiles({
-    files: [PATH],
-    algorithm: 'sha256',
-    noGlob: true
-}, (error, hash) => {
-    if (error) throw error;
+fs.readdirSync(PATH)
+    .filter(f => REGEXP.test(f))
+    .forEach(f => fs.unlinkSync(PATH + f));
 
-    fs.renameSync(PATH, `./../wwwroot/css/styles.${hash.substring(0, 32)}.css`);
-})
+const sassResult = sass.renderSync({
+    file: 'styles.scss',
+    includePaths: ['node_modules'],
+    outputStyle: 'compressed',
+    sourceMap: false,
+    outFile: FILE_PATH
+});
+
+executeAsync().then(() => console.log('Build success'));
+
+async function executeAsync()
+{
+    const postcssResult = await postcss([autoprefixer])
+        .process(sassResult.css.toString(), { from: FILE_PATH, to: FILE_PATH, map: false });
+
+    const purgeCssResult = await new PurgeCSS().purge({
+        content: [
+            './../../Client.Online/wwwroot/index.html',
+            './../../Client.Pwa/wwwroot/index.html',
+            './../../Client.Shared/**/*.razor',
+            './../../Client.Online/**/*.razor',
+            './../../Client.Pwa/**/*.razor',
+        ],
+        safelist: {
+            greedy: [/fade-container/, /bg-info/, /bg-warning/, /bg-danger/]
+        },
+        keyframes: true,
+        output: './../wwwroot/css/',
+        css: [
+            { raw: postcssResult.css }
+        ]
+    });
+
+    const hash = createHash('sha256');
+    hash.update(purgeCssResult[0].css);
+    const digest = hash.digest('hex').toString().substring(0, HASH_LENGTH);
+
+    fs.writeFileSync(`${PATH}styles.${digest}.css`, purgeCssResult[0].css);
+}
