3c199db4cb61d4411f5948e89ac083ef89b793af
codernr <zoltan.csore@protonmail.com>
2021-06-14 19:43:51 +0200

    #31 Leave hash for now, use styles.css

--- Client.Shared/Npm/build.js
+++ Client.Shared/Npm/build.js
@@ -3,23 +3,15 @@ const sass = require('sass');
 const autoprefixer = require('autoprefixer');
 const postcss = require('postcss');
 const { PurgeCSS } = require('purgecss');
-const { createHash } = require('crypto');
 
-const PATH = './../wwwroot/css/';
-const FILE_PATH = `${PATH}styles.css`;
-const HASH_LENGTH = 32;
-const REGEXP = new RegExp(`styles.[a-z0-9]{${HASH_LENGTH}}.css$`, 'g');
-
-fs.readdirSync(PATH)
-    .filter(f => REGEXP.test(f))
-    .forEach(f => fs.unlinkSync(PATH + f));
+const PATH = './../wwwroot/css/styles.css';
 
 const sassResult = sass.renderSync({
     file: 'styles.scss',
     includePaths: ['node_modules'],
     outputStyle: 'compressed',
     sourceMap: false,
-    outFile: FILE_PATH
+    outFile: PATH
 });
 
 executeAsync().then(() => console.log('Build success'));
@@ -27,7 +19,7 @@ executeAsync().then(() => console.log('Build success'));
 async function executeAsync()
 {
     const postcssResult = await postcss([autoprefixer])
-        .process(sassResult.css.toString(), { from: FILE_PATH, to: FILE_PATH, map: false });
+        .process(sassResult.css.toString(), { from: PATH, to: PATH, map: false });
 
     const purgeCssResult = await new PurgeCSS().purge({
         content: [
@@ -47,9 +39,5 @@ async function executeAsync()
         ]
     });
 
-    const hash = createHash('sha256');
-    hash.update(purgeCssResult[0].css);
-    const digest = hash.digest('hex').toString().substring(0, HASH_LENGTH);
-
-    fs.writeFileSync(`${PATH}styles.${digest}.css`, purgeCssResult[0].css);
+    fs.writeFileSync(PATH, purgeCssResult[0].css);
 }
