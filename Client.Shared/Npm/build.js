53ad6d984a293b908eff89b4497e4b0a0f0bd0de
codernr <zoltan.csore@protonmail.com>
2021-06-15 02:12:08 +0200

    Create build tests

--- Client.Shared/Npm/build.js
+++ Client.Shared/Npm/build.js
@@ -4,18 +4,23 @@ const autoprefixer = require('autoprefixer');
 const postcss = require('postcss');
 const { PurgeCSS } = require('purgecss');
 
-const PATH = './../wwwroot/css/styles.css';
+const PATH = './../wwwroot/css/';
+const DEFAULT_FILEPATH = PATH + 'styles.css';
 
 const release = process.argv.includes('Release');
 
-if (fs.existsSync(PATH)) fs.unlinkSync(PATH);
+const regex = /^styles(?:\.?[a-z0-9]{32})?\.css$/;
+
+fs.readdirSync(PATH)
+    .filter(f => regex.test(f))
+    .forEach(f => fs.unlinkSync(PATH + f));
 
 const sassResult = sass.renderSync({
     file: 'styles.scss',
     includePaths: ['node_modules'],
     outputStyle: 'compressed',
     sourceMap: false,
-    outFile: PATH
+    outFile: DEFAULT_FILEPATH
 });
 
 executeAsync().then(() => console.log('Build success'));
@@ -23,7 +28,7 @@ executeAsync().then(() => console.log('Build success'));
 async function executeAsync()
 {
     const postcssResult = await postcss([autoprefixer])
-        .process(sassResult.css.toString(), { from: PATH, to: PATH, map: false });
+        .process(sassResult.css.toString(), { from: DEFAULT_FILEPATH, to: DEFAULT_FILEPATH, map: false });
 
     const purgeCssResult = await new PurgeCSS().purge({
         content: [
@@ -43,5 +48,17 @@ async function executeAsync()
         ]
     });
 
-    fs.writeFileSync(PATH, purgeCssResult[0].css);
+    let outputPath = DEFAULT_FILEPATH;
+
+    if (release) {
+        const { createHash } = require('crypto');
+
+        const hash = createHash('sha256');
+        hash.update(purgeCssResult[0].css);
+        const digest = hash.digest('hex');
+
+        outputPath = outputPath.replace('.css', `.${digest.substring(0,32)}.css`);
+    }
+
+    fs.writeFileSync(outputPath, purgeCssResult[0].css);
 }
