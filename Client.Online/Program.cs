6d1f864bdfe2cd55f327f13a921c8ae1828ac077
codernr <zoltan.csore@protonmail.com>
2021-11-19 17:13:02 +0100

    #32 Update blazor apps

--- Client.Online/Program.cs
+++ Client.Online/Program.cs
@@ -1,41 +1,34 @@
 using System;
 using System.Net.Http;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Fishbowl.Net.Client.Shared;
 using System.Globalization;
 using Microsoft.Extensions.Logging;
 using Fishbowl.Net.Client.Online.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Fishbowl.Net.Client.Online;
 
-namespace Fishbowl.Net.Client.Online
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-            builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 
-            builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder
-                .AddInteropServices()
-                .Services
-                    .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
-                    .AddTransient<IClientState, ClientState>()
-                    .AddLocalization();
+builder
+    .AddInteropServices()
+    .Services
+        .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+        .AddTransient<IClientState, ClientState>()
+        .AddLocalization();
 
-            var culture = new CultureInfo("hu");
-            CultureInfo.DefaultThreadCurrentCulture = culture;
-            CultureInfo.DefaultThreadCurrentUICulture = culture;
+var culture = new CultureInfo("hu");
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
 
-            var host = builder.Build();
+var host = builder.Build();
 
-            await host.InitializeInteropServicesAsync();
-            
-            await host.RunAsync();
-        }
-    }
-}
+await host.InitializeInteropServicesAsync();
+
+await host.RunAsync();
--- Client.Pwa/Program.cs
+++ Client.Pwa/Program.cs
@@ -1,35 +1,28 @@
 using System.Globalization;
-using System.Threading.Tasks;
 using Fishbowl.Net.Client.Pwa.Common;
 using Fishbowl.Net.Client.Shared;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Fishbowl.Net.Client.Pwa;
+using Microsoft.AspNetCore.Components.Web;
 
-namespace Fishbowl.Net.Client.Pwa
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder
-                .AddInteropServices()
-                .Services
-                    .AddLocalization()
-                    .AddJsonSerializationOptions()
-                    .AddSingleton<GameProperty>();
+builder
+    .AddInteropServices()
+    .Services
+        .AddLocalization()
+        .AddJsonSerializationOptions()
+        .AddSingleton<GameProperty>();
 
-            var culture = new CultureInfo("hu");
-            CultureInfo.DefaultThreadCurrentCulture = culture;
-            CultureInfo.DefaultThreadCurrentUICulture = culture;
+var culture = new CultureInfo("hu");
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
 
-            var host = builder.Build();
+var host = builder.Build();
 
-            await host.InitializeInteropServicesAsync();
-            
-            await host.RunAsync();
-        }
-    }
-}
+await host.InitializeInteropServicesAsync();
+
+await host.RunAsync();
