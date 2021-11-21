ca425bd4b7d8dcae5820444cdf614a182bdbebf4
codernr <zoltan.csore@protonmail.com>
2021-11-21 16:43:56 +0100

    #35 Update PlayerWords

--- Client.Shared/Components/States/PlayerWords.razor.cs
+++ Client.Shared/Components/States/PlayerWords.razor.cs
@@ -7,15 +7,29 @@ namespace Fishbowl.Net.Client.Shared.Components.States
         [Parameter]
         public EventCallback<string[]> OnPlayerWordsSet { get; set; } = default!;
 
-        public int WordCount {
+        public int WordCount
+        {
             get => this.wordCount;
             set
             {
                 this.wordCount = value;
                 this.words = new string[value];
-            } }
+            }
+        }
+
+        private bool IsValid
+        {
+            get => this.isValid;
+            set
+            {
+                this.isValid = value;
+                this.Update();
+            }
+        }
 
         private int wordCount = 2;
+
+        private bool isValid = false;
 
         private string[] words = new string[2];
     }
