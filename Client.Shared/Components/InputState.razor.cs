ea5179a389a824ee6705f08d9fac675d4d69523a
codernr <zoltan.csore@protonmail.com>
2021-11-24 00:17:53 +0100

    #42 Update InputState

--- Client.Shared/Components/InputState.razor.cs
+++ Client.Shared/Components/InputState.razor.cs
@@ -3,16 +3,9 @@ using System.Threading.Tasks;
 
 namespace Fishbowl.Net.Client.Shared.Components
 {
-    public abstract class InputState : State
+    public abstract class InputState
     {
         protected bool submitted = false;
-
-        public override Task EnableAsync()
-        {
-            this.submitted = false;
-            this.Update();
-            return base.EnableAsync();
-        }
 
         protected virtual Task Submit(Func<Task> submitAction)
         {
