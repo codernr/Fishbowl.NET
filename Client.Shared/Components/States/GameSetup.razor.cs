17ef1c65b2172c4025047703ca4422a40cd32537
codernr <zoltan.csore@protonmail.com>
2021-11-23 11:20:42 +0100

    #40 Create GameSetup, update resources

---
+++ Client.Shared/Components/States/GameSetup.razor.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fishbowl.Net.Shared.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace Fishbowl.Net.Client.Shared.Components.States
+{
+    public partial class GameSetup
+    {
+        [Parameter]
+        public EventCallback<GameSetupViewModel> OnGameSetup { get; set; } = default!;
+
+        private bool IsValid
+        {
+            get => this.isValid;
+            set
+            {
+                this.isValid = value;
+                this.Update();
+            }
+        }
+
+        private int PlayerCount
+        {
+            get => this.playerCount;
+            set
+            {
+                this.playerCount = value;
+                this.Update();
+            }
+        }
+
+        private int MaxTeamCount => this.PlayerCount / 2;
+
+        private bool isValid = false;
+
+        private int playerCount = 4;
+
+        private int teamCount = 2;
+
+        private int wordCount = 1;
+
+        private IEnumerable<string> roundTypes = default!;
+
+        private IEnumerable<string> selectedRoundTypes = default!;
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            this.roundTypes = new[] { "Taboo", "Drawing", "Charades", "Password", "Humming" }
+                .Select(item => L[$"Components.States.GameSetup.RoundTypes.{item}"].Value);
+        }
+
+        private Task SetupGame() =>
+            this.Submit(() => this.OnGameSetup.InvokeAsync(
+                new(this.PlayerCount, this.wordCount, this.teamCount, this.selectedRoundTypes.ToArray())));
+    }
+}
