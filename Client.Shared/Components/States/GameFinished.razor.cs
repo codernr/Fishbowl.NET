c5cf7f09bf8f539f02614417e0bc0d9391a1bf09
codernr <zoltan.csore@protonmail.com>
2021-11-25 08:17:27 +0100

    #36 Display winner team(s)

--- Client.Shared/Components/States/GameFinished.razor.cs
+++ Client.Shared/Components/States/GameFinished.razor.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Fishbowl.Net.Shared.ViewModels;
 
 namespace Fishbowl.Net.Client.Shared.Components.States
@@ -8,5 +10,26 @@ namespace Fishbowl.Net.Client.Shared.Components.States
         public Action ReloadRequested { get; set; } = default!;
         
         public GameSummaryViewModel Game { get; set; } = default!;
+
+        private Dictionary<string, int> teamScores = default!;
+
+        private string[] winnerTeams = default!;
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            this.teamScores = this.Game.Teams
+                .ToDictionary(
+                    team => team.Name,
+                    team => team.Players.Sum(player => player.Scores.Count));
+
+            var max = this.teamScores.Max(team => team.Value);
+
+            this.winnerTeams = this.teamScores
+                .Where(team => team.Value == max)
+                .Select(team => team.Key)
+                .ToArray();
+        }
     }
 }
