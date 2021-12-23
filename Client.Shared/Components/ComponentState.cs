bccaa6c34925e2e8d89d02a1299bb1169d7d0dc1
codernr <zoltan.csore@protonmail.com>
2021-12-23 14:54:52 +0100

    #55 Refactor ComponentState

--- Client.Shared/Components/ComponentState.cs
+++ Client.Shared/Components/ComponentState.cs
@@ -1,12 +1,13 @@
 using System;
 using Fishbowl.Net.Client.Shared.I18n;
 using Fishbowl.Net.Client.Shared.Services;
+using Fluxor;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Localization;
 
 namespace Fishbowl.Net.Client.Shared.Components
 {
-    public abstract class ComponentState<T> : ComponentBase where T : ComponentState<T>
+    public abstract class ComponentState<T> : ComponentBase, IDisposable where T : class
     {
         [Inject]
         public IStringLocalizer<Resources> L { get; set; } = default!;
@@ -14,8 +15,8 @@ namespace Fishbowl.Net.Client.Shared.Components
         [Inject]
         public AppStateService AppState { get; set; } = default!;
 
-        [Parameter]
-        public Action<T>? SetParameters { get; set; }
+        [Inject]
+        public IState<T> State { get; set; } = default!;
 
         public string Title
         {
@@ -29,21 +30,17 @@ namespace Fishbowl.Net.Client.Shared.Components
 
         private string title = default!;
 
-        public void Update(Action<T> updateParameters)
-        {
-            updateParameters(this.Instance);
-            this.StateHasChanged();
-        }
-
-        private T Instance => this as T ?? throw new InvalidCastException();
-
         protected override void OnInitialized()
         {
             base.OnInitialized();
 
             this.Title = this.L[$"Components.States.{this.GetType().Name}.Title"];
 
-            this.SetParameters?.Invoke(this.Instance);
+            this.State.StateChanged += this.StateChanged;
         }
+
+        protected virtual void StateChanged(object? sender, T state) => this.InvokeAsync(this.StateHasChanged);
+
+        public void Dispose() => this.State.StateChanged -= this.StateChanged;
     }
 }
