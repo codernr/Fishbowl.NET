afe78a2cadf37139846fc0d050f6fdfef40d7b9a
codernr <zoltan.csore@protonmail.com>
2021-12-25 22:11:38 +0100

    #55 Rename StateContainer to ScreenContainer

--- Client.Shared/Components/Templates/ScreenContainer.razor.cs
+++ Client.Shared/Components/Templates/ScreenContainer.razor.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Components;
 
 namespace Fishbowl.Net.Client.Shared.Components.Templates
 {
-    public partial class StateContainer
+    public partial class ScreenContainer
     {
         [Parameter]
         public RenderFragment ChildContent { get; set; } = default!;
