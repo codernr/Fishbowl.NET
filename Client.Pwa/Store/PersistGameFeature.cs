de1579e648e48c8b5b6dc2f75ba2e9d04ea54fcc
codernr <zoltan.csore@protonmail.com>
2021-12-25 13:54:05 +0100

    #55 Fix game persistence

--- Client.Pwa/Store/PersistGameFeature.cs
+++ Client.Pwa/Store/PersistGameFeature.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Fishbowl.Net.Client.Pwa.Common;
 using Fishbowl.Net.Client.Shared.Store;
@@ -5,25 +7,27 @@ using Fluxor;
 
 namespace Fishbowl.Net.Client.Pwa.Store
 {
-    public class PersistGameEffects
+    public class PersistGameEffects : IEffect
     {
         private readonly GameProperty persistedGame;
 
         private readonly IState<GamePlayState> state;
 
+        private static readonly List<Type> Actions = new()
+        {
+            typeof(StartGameAction), typeof(SetPeriodSetupPlayAction), typeof(SetPeriodPlayPeriodAction),
+            typeof(SetPeriodFinishedAction), typeof(SetPeriodPlayScoreCountAction)
+        };
+
         public PersistGameEffects(GameProperty persistedGame, IState<GamePlayState> state) =>
             (this.persistedGame, this.state) = (persistedGame, state);
 
-        [EffectMethod(typeof(StateManagerTransitionAction))]
-        public Task OnStartStateManagerTransitionAction(IDispatcher dispatcher) => this.Persist();
-
-        [EffectMethod(typeof(SetPeriodPlayScoreCountAction))]
-        public Task OnSetPeriodPlayScoreCount(IDispatcher dispatcher) => this.Persist();
-
-        private Task Persist()
+        public Task HandleAsync(object action, IDispatcher dispatcher)
         {
             this.persistedGame.Value = this.state.Value.Game?.Game;
             return Task.CompletedTask;
         }
+
+        public bool ShouldReactToAction(object action){System.Console.WriteLine(action.GetType().ToString() + Actions.Contains(action.GetType())); return Actions.Contains(action.GetType()); }
     }
 }
