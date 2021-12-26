8ea41482c1a9b5f769a8f2f72e46cd2d59667777
codernr <zoltan.csore@protonmail.com>
2021-12-26 18:10:15 +0100

    #55 Add overlay on transition

--- Client.Shared/Store/ScreenManagerFeature.cs
+++ Client.Shared/Store/ScreenManagerFeature.cs
@@ -9,6 +9,8 @@ namespace Fishbowl.Net.Client.Shared.Store
     public record ScreenManagerState
     {
         public string Title { get; init; } = string.Empty;
+
+        public bool IsTransitioning { get; init; }
     }
 
     public record SetScreenManagerTitleAction(string Title);
@@ -17,13 +19,21 @@ namespace Fishbowl.Net.Client.Shared.Store
 
     public record ScreenManagerTransitionAction(Type NextState, object? InterceptorAction = null, TimeSpan Delay = default);
 
-    public record ScreenManagerTransitionEndAction(Type CurrentState);
+    public record ScreenManagerTransitionEndAction();
 
     public static class ScreenManagerReducers
     {
         [ReducerMethod]
         public static ScreenManagerState OnSetScreenManagerTitle(ScreenManagerState state, SetScreenManagerTitleAction action) =>
             state with { Title = action.Title };
+
+        [ReducerMethod(typeof(ScreenManagerTransitionAction))]
+        public static ScreenManagerState OnScreenManagerTransition(ScreenManagerState state) =>
+            state with { IsTransitioning = true };
+
+        [ReducerMethod(typeof(ScreenManagerTransitionEndAction))]
+        public static ScreenManagerState OnScreenManagerTransitionEnd(ScreenManagerState state) =>
+            state with { IsTransitioning = false };
     }
 
     public class ScreenManagerEffects
@@ -38,7 +48,10 @@ namespace Fishbowl.Net.Client.Shared.Store
         }
 
         [EffectMethod]
-        public Task OnScreenManagerTransition(ScreenManagerTransitionAction action, IDispatcher dispatcher) =>
-            this.screenManager.SetScreen(action.NextState, action.InterceptorAction, action.Delay);
+        public async Task OnScreenManagerTransition(ScreenManagerTransitionAction action, IDispatcher dispatcher)
+        {
+            await this.screenManager.SetScreen(action.NextState, action.InterceptorAction, action.Delay);
+            dispatcher.Dispatch(new ScreenManagerTransitionEndAction());
+        }
     }
 }
